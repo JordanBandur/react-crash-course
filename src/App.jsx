d2a9918e923d38ae0aa4e7f11798cedf803be8c7
JordanBandur <jordanbandur@hotmail.ca>
2024-07-08 10:02:22 -0600

    Update Post to use props

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import Post from './components/Post';
 
 function App() {
-  return <Post />;
+  return (
+    <main>
+      <Post author="Jordan" body="I'm refreshing my React knowledge!"/>
+      <Post author="Dylan" body="I'm learning to play the guitar!"/>
+    </main>
+  );
 }
 
 export default App;
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,12 +1,11 @@
-const names = ['Jordan', 'Dylan'];
+function Post(props) {
 
-function Post() {
-  const chosenName = Math.random() > 0.5 ? names[0] : names[1];
+  const { author, body } = props;
 
   return (
     <div>
-      <p>{chosenName}</p>
-      <p>React is awesome!</p>
+      <p>{author}</p>
+      <p>{body}</p>
     </div>
   );
 }
