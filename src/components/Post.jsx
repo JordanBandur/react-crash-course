ea7fec5f839b39d54e589380916ffd744096ce6b
JordanBandur <jordanbandur@hotmail.ca>
2024-07-07 21:48:20 -0600

    Add Post component

---
+++ src/components/Post.jsx
@@ -0,0 +1,14 @@
+const names = ['Jordan', 'Dylan'];
+
+function Post() {
+  const chosenName = Math.random() > 0.5 ? names[0] : names[1];
+
+  return (
+    <div>
+      <p>{chosenName}</p>
+      <p>React is awesome!</p>
+    </div>
+  );
+}
+
+export default Post;
