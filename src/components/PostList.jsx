f5af2dbc5b4ac3a2c0d85d43c7edb153aefd44c1
JordanBandur <jordanbandur@hotmail.ca>
2024-07-09 08:09:00 -0600

    Import NewPost component

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,12 +1,16 @@
 import Post from "./Post";
+import NewPost from "./NewPost";
 import classes from "./PostList.module.css";
 
 function PostList() {
   return (
-    <ul>
-      <Post author="Jordan" body="I'm refreshing my React knowledge!" />
-      <Post author="Dylan" body="I'm learning to play the guitar!" />
-    </ul>
+    <>
+      <NewPost />
+      <ul>
+        <Post author="Jordan" body="I'm refreshing my React knowledge!" />
+        <Post author="Dylan" body="I'm learning to play the guitar!" />
+      </ul>
+    </>
   );
 }
 
