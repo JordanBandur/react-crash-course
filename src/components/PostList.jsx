4e8dcdbe49f549da2c28fc441747347ee2c30865
JordanBandur <jordanbandur@hotmail.ca>
2024-07-09 14:37:19 -0600

    Lift NewPost state up to PostList

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,13 +1,25 @@
+import { useState } from 'react';
 import Post from "./Post";
 import NewPost from "./NewPost";
 import classes from "./PostList.module.css";
 
 function PostList() {
+  const [bodyText, setBodyText] = useState('');
+  const [authorText, setAuthorText] = useState('');
+
+  function changeBodyHandler(event) {
+    setBodyText(event.target.value);
+  }
+
+  function changeAuthorHandler(event) {
+    setAuthorText(event.target.value);
+  }
+
   return (
     <>
-      <NewPost />
-      <ul>
-        <Post author="Jordan" body="I'm refreshing my React knowledge!" />
+      <NewPost onBodyChange={changeBodyHandler} onAuthorChange={changeAuthorHandler} />
+      <ul className={classes.posts}>
+        <Post author={authorText} body={bodyText} />
         <Post author="Dylan" body="I'm learning to play the guitar!" />
       </ul>
     </>
