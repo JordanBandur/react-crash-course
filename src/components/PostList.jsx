09a604c2edbfe85febc1fa608373bb40521abc55
JordanBandur <jordanbandur@hotmail.ca>
2024-07-10 08:27:03 -0600

    Update code to allow state updates with prev

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,9 +1,17 @@
+import { useState } from "react";
 import Post from "./Post";
 import NewPost from "./NewPost";
 import classes from "./PostList.module.css";
 import Modal from './Modal';
 
 function PostList({ isPosting, onStopPosting }) {
+  const [posts, setPosts] = useState([]);
+
+  function addPostHandler(postData) {
+    setPosts((prev) => [postData, ...prev]); // React does not excute state updating functions instantly
+    // using prev gets the current snapshot(prev) of the state and allows you update state with the
+    // previous snapshot
+  }
 
   return (
     <>
@@ -11,6 +19,7 @@ function PostList({ isPosting, onStopPosting }) {
         <Modal onClose={onStopPosting}>
           <NewPost
             onCancel={onStopPosting}
+            onAddPost={addPostHandler}
           />
         </Modal>
       )}
