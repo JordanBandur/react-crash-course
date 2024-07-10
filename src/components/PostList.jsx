f0e3290fbab343833b2bf670cc748c94282a4dee
JordanBandur <jordanbandur@hotmail.ca>
2024-07-10 08:41:22 -0600

    Add conditional rendering based of # of posts

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -23,9 +23,18 @@ function PostList({ isPosting, onStopPosting }) {
           />
         </Modal>
       )}
-      <ul className={classes.posts}>
-        {posts.map(() => <Post key={posts.body} author={posts.author} body={posts.body} />)}
-      </ul>
+      {posts.length > 0 && (
+        <ul className={classes.posts}>
+          {posts.map((post) => (<Post key={posts.body} author={posts.author} body={posts.body} />
+          ))}
+        </ul>
+      )}
+      {post.length === 0 && (
+        <div style={{ textAlign: 'center', color: 'white' }}>
+          <h2>There are no posts yet.</h2>
+          <p>Start adding some!</p>
+        </div>
+      )}
     </>
   );
 }
