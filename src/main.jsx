7b1d89a77f4309bea7f77ad7adaf888e10d116be
JordanBandur <jordanbandur@hotmail.ca>
2024-07-11 10:25:50 -0600

    Add layout routes

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,21 @@ import ReactDOM from 'react-dom/client';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import App from './App';
 import NewPost from './components/NewPost';
+import RootLayout from './routes/RootLayout';
 import './index.css';
 
 const router = createBrowserRouter([
-  {path: '/', element: <App />},
-  {path: '/create-post', element: <NewPost />},
+  {
+    path: '/', element: <RootLayout />,
+    children: [
+      { path: '/', element: <App /> },
+      { path: '/create-post', element: <NewPost /> },
+    ]
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider  router={router}/> {/*enables routing and renders components based off url path */}
+    <RouterProvider router={router} /> {/*enables routing and renders components based off url path */}
   </React.StrictMode>
 );
